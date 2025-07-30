ad553d7b05baabc8f8e5c66ba7bc3ab49633955a
Avellin003 <uavellinbonaparte@gmail.com>
2025-07-30 07:55:17 +0200

    updates

--- KoraQuest/settings.py
+++ KoraQuest/settings.py
@@ -167,8 +167,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'b.uteramaho@alustudent.com'
-EMAIL_HOST_PASSWORD = 'tglb sxys nmfq wxgc'
+EMAIL_HOST_USER = 'enter-your-email'
+EMAIL_HOST_PASSWORD = 'enter-your-email-host-password'
 
 # Default from email (required for sending emails)
 # DEFAULT_FROM_EMAIL = 'KoraQuest <noreply@koraquest.com>'
